11cb7a8fa0f34e5e0ce821333424b6bf5fb26e82
rituchowdhury425-ops <rituchowdhury425@gmail.com>
2026-07-09 17:12:12 +0530

    Reduce GIF size

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageOps
 import os
 
 # Folder names
@@ -12,20 +12,35 @@ images = []
 for file in sorted(os.listdir(image_folder)):
     if file.endswith((".jpg", ".jpeg", ".png")):
         path = os.path.join(image_folder, file)
+
         img = Image.open(path)
-        images.append(img)
+
+        # Resize while keeping aspect ratio
+        img.thumbnail((150, 150))
+
+        # Create a white background
+        background = Image.new("RGB", (500, 500), "white")
+
+        # Center the image
+        x = (500 - img.width) // 2
+        y = (500 - img.height) // 2
+
+        background.paste(img, (x, y))
+
+        images.append(background)
 
 # Create GIF
 if images:
     output_path = os.path.join(output_folder, "animation.gif")
 
     images[0].save(
-        output_path,
-        save_all=True,
-        append_images=images[1:],
-        duration=500,  # Time each image is displayed (milliseconds)
-        loop=0         # 0 = Loop forever
-    )
+    output_path,
+    save_all=True,
+    append_images=images[1:],
+    duration=300,
+    loop=0,
+    optimize=True
+)
 
     print("GIF created successfully!")
 else:
